5b211704017057edb3db2dc547dbcaae0d76d2f4
fgaudin <fg@cgcom.com>
2015-08-14 14:40:56 -0400

    int. styleguide

--- Gruntfile.js
+++ Gruntfile.js
@@ -47,7 +47,7 @@ module.exports = function(grunt){
           expand: true,                  // Enable dynamic expansion 
           cwd: 'src/',                   // Src matches are relative to this path 
           src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match 
-          dest: 'images/'                // Destination path prefix
+          dest: ''                // Destination path prefix
         }]
       }
     },
